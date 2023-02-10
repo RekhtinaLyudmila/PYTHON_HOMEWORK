993e185efd3ca10795bc096555a7cc48dbb59cd1
Rekhtina Lyudmila <Rehtina520@mail.ru>
2023-02-10 22:11:13 +0700

    ADD 7_4

--- Lesson7/7_4.py
+++ Lesson7/7_4.py
@@ -1,3 +1,16 @@
+'''Реализуйте базовый класс Car. У данного класса должны быть следующие публичные атрибуты:
+speed, color, name, is_police (булево).
+А также публичные методы: go, stop, turn(direction),
+которые должны сообщать, что машина поехала, остановилась, повернула (куда).
+Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс публичный метод show_speed,
+который должен показывать текущую скорость автомобиля.
+Для классов TownCar и WorkCar переопределите метод show_speed.
+При значении скорости свыше 60 (TownCar)
+и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+Создайте экземпляры классов, передайте значения атрибутов.
+Выполните доступ к атрибутам, выведите результат.
+Выполните вызов методов и также покажите результат.'''
 
 class Car:
     # atributes
@@ -64,16 +77,16 @@ class PoliceCar(Car):
 
 
 audi = SportCar(100, 'Red', 'Audi', False)
-oka = TownCar(30, 'White', 'Oka', False)
-lada = WorkCar(70, 'Rose', 'Lada', True)
+renault = TownCar(30, 'White', 'Renault', False)
+lada = WorkCar(70, 'Green', 'Lada', False)
 ford = PoliceCar(110, 'Blue',  'Ford', True)
 print(lada.turn_left())
-print(f'When {oka.turn_right()}, then {audi.stop()}')
+print(f'When {renault.turn_right()}, then {audi.stop()}')
 print(f'{lada.go()} with speed exactly {lada.show_speed()}')
 print(f'{lada.name} is {lada.color}')
 print(f'Is {audi.name} a police car? {audi.is_police}')
-print(f'Is {lada.name}  a police car? {lada.is_police}')
+print(f'Is {ford.name}  a police car? {ford.is_police}')
 print(audi.show_speed())
-print(oka.show_speed())
+print(renault.show_speed())
 print(ford.police())
 print(ford.show_speed())
