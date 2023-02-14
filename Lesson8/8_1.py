66b6b78b27ca40788220ee919182352290e13a7c
Rekhtina Lyudmila <Rehtina520@mail.ru>
2023-02-14 18:06:41 +0700

    ADD 8_1

---
+++ Lesson8/8_1.py
@@ -0,0 +1,20 @@
+'''Создать программно файл в текстовом формате,
+записать в него построчно данные, вводимые пользователем.
+Об окончании ввода данных свидетельствует пустая строка.'''
+
+my_list = []
+with open("myfile.txt", "w") as file_obj:
+        file_obj.writelines(my_list)
+while True:
+    line = input("Введите произвольную строку: ")
+    if line == '':
+        print(my_list)
+        exit()
+    else:
+        newline = line + '\n'
+        my_list.append(newline)
+        
+       
+
+    
+
