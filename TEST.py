42af169266dedc391c7cb33dbceda14a45e7f6b0
Rekhtina Lyudmila <Rehtina520@mail.ru>
2023-02-17 15:15:58 +0700

    ADD 8_3

--- TEST.py
+++ TEST.py
@@ -1,12 +1,16 @@
-my_file = open('teхt_2.txt', 'r')
-content = my_file.read()
-print(f'Содержимое файла: \n {content}')
-try:   
-    with open ('teхt_2.txt'):
-    lines = 0
-    letters = 0
-    for line in file_obj:
-        lines += line.count("\n")
-        letters = len(line)-1
-        print(f"{letters} letters in line")
-    print(f"String count is {lines}")
+l = ['один', 'два', 'три', 'четыре', 'пять']
+l2 = []
+f1 = open('text_3.txt')
+a = f1.readlines()
+print(a)
+e = len(a)
+j = 0
+for i in a:
+    b = i
+    c = i.find(' ')
+    d = i[:c]
+    r = b.replace(d, l[j])
+    j = j + 1
+l2.append(r)
+f2 = open('text_3_new.txt', 'w')
+f2.writelines(l2)
