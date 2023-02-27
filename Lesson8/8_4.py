71d6e9f9b4a946996f169c2a0489893bc0fc08b3
Rekhtina Lyudmila <Rehtina520@mail.ru>
2023-02-27 19:27:12 +0700

    ADD 8_4

---
+++ Lesson8/8_4.py
@@ -0,0 +1,37 @@
+'''Создать функцию write_order_to_json(), в которую передается
+5 параметров — товар (item), количество (quantity), цена (price),
+покупатель (buyer), дата (date). Функция должна предусматривать запись
+данных в виде словаря в файл orders.json. При записи данных указать
+величину отступа в 4 пробельных символа.'''
+
+
+
+import os
+import json
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    filename = os.path.join(CURRENT_DIR,  'orders.json')
+
+    if os.path.exists(filename):
+        data = {}
+
+        with open(filename, encoding="utf-8") as fl:
+            data = json.loads(fl.read())
+
+        data['orders'].append({'item': item, 'quantity': quantity, 'price': price, 'buyer': buyer, 'date': date})
+
+        with open(filename, "w", encoding="utf-8") as fl:
+            json.dump(data, fl, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+        print(f'Данные добавлены в {filename}')
+
+    else:
+        print(f'Исходный файл по пути {filename} не найден')
+
+
+if __name__ == '__main__':
+    write_order_to_json('scaner', '20', '10000', 'Petrov P.P.', '11.01.2018')
+    
